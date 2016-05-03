2542bb2d1b164662d20c61caeb544db23cef8f41
Njenga Kariuki <njengak@me.com>
2016-05-02 19:23:22 -0700

    whatever

---
+++ 03_Ruby_Fundamentals/exercises/love_post.rb
@@ -0,0 +1,25 @@
+
+def cupid
+  puts "Please input who your love is"
+  love_interest = gets.chomp
+  binding.pry
+ puts "Your love interest is #{love_interest}"
+ case
+when they_said = "Njenga"
+  puts "Great choice!"
+when they_said = "Ben"
+  puts "Bad choice!"
+end
+end
+
+
+
+
+
+
+
+
+
+
+#####
+puts cupid
