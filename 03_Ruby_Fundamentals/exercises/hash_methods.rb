67d896c1d77fae565d7bb8323084cc5bf5a0c979
Njenga Kariuki <njengak@me.com>
2016-05-04 18:25:33 -0700

    saving homework and notes

---
+++ 03_Ruby_Fundamentals/exercises/hash_methods.rb
@@ -0,0 +1,34 @@
+
+item = {}
+item[:diet_coke] = { size: 20, size_type: "ounce"}
+
+item[:sprite_single] = { size:24, size_type: "ounce"}
+
+cars = {}
+cars= {"tesla" => 80000, "ford" => 30000 }
+
+
+categories = {}
+categories["sports"] = {vendor_manager: "Brandon Ingram", year_started: "2012"}
+categories["home"] = {vendor_manager: "Katie Bulloch", year_started: "2016"}
+
+#####PRACTICE#####
+
+##length : Will tell you the number of key/value pairs
+puts "The .length method will tell you the number of key/value pairs you have. In this case I have #{item.length}"
+
+#.merge -- will comebine two hashes into one
+puts "The .merge will allow you to combine two hashes into one. Here is an example that combines Items into categories: #{item.merge(categories)}"
+
+# select {} allows  you to filter values a hash for specific criteria
+puts cars.select {|car, price| price > 20000 && car.eql?("tesla")}
+puts categories.select {|cat,vm| cat.eql?("home")}
+
+## .keys -- will return all the keys associated with the hash
+puts "For the hash categories the .keys method can be used to show them. See! #{categories.keys}"
+
+## .values --  returns the values associated with the hash
+puts "For the hash categories the .values method can be used to show them. See! #{categories.values}"
+
+##.has_key -- allows you to check, if a certain object exists in a hash
+puts "The .has_keys can be used to search for a key in a hash. Let's see if I have a life: #{categories.has_key?("life")}"
--- 03_Ruby_Fundamentals/exercises/love_post.rb
+++ 03_Ruby_Fundamentals/exercises/love_post.rb
@@ -1,17 +1,31 @@
+require 'pry'
+
 
 def cupid
-  puts "Please input who your love is"
-  love_interest = gets.chomp
-  binding.pry
- puts "Your love interest is #{love_interest}"
- case
-when they_said = "Njenga"
-  puts "Great choice!"
-when they_said = "Ben"
-  puts "Bad choice!"
-end
+  puts "Please input who your love is?"
+  love_interest = gets.strip
+  cupid_love(love_interest)
 end
 
+def cupid_love(love_interest)
+  puts "Do you truly love #{love_interest}?"
+  puts "Answer 'Absolutely' or 'I lied'"
+  answer = gets.strip
+  puts "For real?! #{answer}"
+  cupid_advice(answer, love_interest)
+end
+
+def cupid_advice(answer, love_interest)
+  case answer
+  when "Absolutely"
+    puts "I am happy for you."
+  when "I lied"
+    puts "Stop lying, I hate you"
+  else "You dont know how to read"
+    cupid_love(love_interest)
+  end
+
+end
 
 
 
@@ -23,3 +37,4 @@ end
 
 #####
 puts cupid
+#puts cupid_advice(answer, love_interest)
